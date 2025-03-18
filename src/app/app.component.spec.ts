897c8aa6990eb57af9919662e3da0cc9f049b1b0
nqluong <nguyenluong052004@gmail.com>
2025-03-18 15:26:03 +0700

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,27 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontend');
-=======
-  it(`should have the 'QL_KhachSan' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('QL_KhachSan');
->>>>>>> test
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
-=======
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, QL_KhachSan');
->>>>>>> test
   });
 });
