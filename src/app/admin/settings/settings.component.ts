2a0f3f0205b223ea0a9ab05c8c59bab3752de6f3
nhatplbn2k4 <nhat6a4@gmail.com>
2025-03-22 14:26:18 +0700

    update

--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ScriptService } from '../../services/script.service';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,18 +8,5 @@ import { RouterModule } from '@angular/router';
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
-export class SettingsComponent implements OnInit {
-
-  constructor(private scriptService: ScriptService) { }
-
-  ngOnInit() {
-    const adminScripts = [
-      'assets/admin/js/jquery-3.5.1.min.js',
-      'assets/admin/js/popper.min.js',
-      'assets/admin/js/bootstrap.min.js',
-      'assets/admin/plugins/slimscroll/jquery.slimscroll.min.js',
-      'assets/admin/js/script.js'
-    ];
-    this.scriptService.load(adminScripts);
-  }
+export class SettingsComponent {
 }
